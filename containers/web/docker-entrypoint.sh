020182db6bbae85188e90a1863ce21dac3531bbb
William Petit <wpetit@cadoles.com>
2016-08-10 14:12:59 +0200

    Infrastructure docker fonctionnelle

--- containers/web/docker-entrypoint.sh
+++ containers/web/docker-entrypoint.sh
@@ -15,15 +15,31 @@ database:
   host: db
 EOF
 
-
-nc -zv db 3306
+nc -zv db 3306 2>/dev/null 1>&2
 while [ "$?" != 0 ]; do
   echo "Waiting for database..."
-  nc -zv db 3306 2>/dev/null 1>&2
   sleep 2
+  nc -zv db 3306 2>/dev/null 1>&2
 done
 
-# Initialize the database
-./script/migrate
+if [ ! -e .initialized ]; then
+
+  echo "Initializing Karambol..."
+
+  ./script/migrate
+  echo "y\n" | ./script/console karambol:rules:seed
+  ./script/console karambol:account:create "${KARAMBOL_ADMIN_USER}" "${KARAMBOL_ADMIN_PASSWORD}"
+  ./script/console karambol:account:promote "${KARAMBOL_ADMIN_USER}"
+
+
+  # Create and update files ACL
+  mkdir -p public/cache
+  chown -R nobody: public/cache
+
+  touch .initialized
+
+fi
+
+run-parts /hooks
 
 /usr/bin/supervisord
